760c518741430df1030382165952531bf8e6e5f8
jreleaserbot <jreleaser@kordamp.org>
2023-07-18 13:44:12 +0000

    riot early-access

--- riot_snapshot.java
+++ riot_snapshot.java
@@ -2,7 +2,7 @@
 //JAVA 17+
 //REPOS jitpack
 //DEPS com.github.redis-developer.riot:riot:main-SNAPSHOT
-// Generated with JReleaser 1.8.0-SNAPSHOT at 2023-07-18T05:44:01.130367129Z
+// Generated with JReleaser 1.8.0-SNAPSHOT at 2023-07-18T13:41:25.573808037Z
 
 public class riot_snapshot {
     public static void main(String... args) throws Exception {
