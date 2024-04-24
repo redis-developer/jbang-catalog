aa4b66671235f1c6a2b916edca57ada4b2de8c22
jreleaserbot <jreleaser@kordamp.org>
2024-04-24 17:42:50 +0000

    riot v3.2.3

--- riot.java
+++ riot.java
@@ -1,7 +1,7 @@
 //usr/bin/env jbang "$0" "$@" ; exit $?
 //JAVA 21+
-//DEPS com.redis:riot:3.2.2
-// Generated with JReleaser 1.12.0-SNAPSHOT at 2024-04-22T21:47:39.315099868Z
+//DEPS com.redis:riot:3.2.3
+// Generated with JReleaser 1.12.0-SNAPSHOT at 2024-04-24T17:37:46.581851101Z
 
 public class riot {
     public static void main(String... args) throws Exception {
